449d1cfb4abc2405b472c85ddad2a0c1ad4bf8da
Laune6 <46917963+Laune6@users.noreply.github.com>
2019-02-05 10:58:31 +0200

    Update chatbot.sql

--- ChatBotWeb/chatbot.sql
+++ ChatBotWeb/chatbot.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 04.02.2019 klo 23:10
--- Palvelimen versio: 5.7.19
--- PHP Version: 5.6.31
+-- Generation Time: Feb 05, 2019 at 08:57 AM
+-- Server version: 5.7.21
+-- PHP Version: 5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `kysymykset`
+-- Table structure for table `kysymykset`
 --
 
 DROP TABLE IF EXISTS `kysymykset`;
@@ -39,7 +39,28 @@ CREATE TABLE IF NOT EXISTS `kysymykset` (
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `vastaukset`
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$R.OkFmxsOBTUgyjGC/Yj9uLREfD3lf6dRTfGfMiHsdwt8bTZKtAcG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vastaukset`
 --
 
 DROP TABLE IF EXISTS `vastaukset`;
