196afc2b862bb947c26d68c442d83bc6ae43ab27
JaniKello <46917315+JaniKello@users.noreply.github.com>
2019-02-19 09:31:34 +0200

    Update chatbot.sql

--- chatbot.sql
+++ chatbot.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 04.02.2019 klo 23:10
--- Palvelimen versio: 5.7.19
--- PHP Version: 5.6.31
+-- Generation Time: Feb 19, 2019 at 07:31 AM
+-- Server version: 5.7.21
+-- PHP Version: 5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `kysymykset`
+-- Table structure for table `kysymykset`
 --
 
 DROP TABLE IF EXISTS `kysymykset`;
@@ -34,20 +34,67 @@ CREATE TABLE IF NOT EXISTS `kysymykset` (
   `Kysymys` text NOT NULL,
   `vID` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kysymykset`
+--
+
+INSERT INTO `kysymykset` (`ID`, `Kysymys`, `vID`) VALUES
+(5, 'fkodkfo', 1),
+(2, 'perkele', 15),
+(3, 'mitÃ¤ ruokaa koulussa', 14),
+(6, 'paljon kanan kilohinta', 14),
+(7, 'kukkakaali', 5),
+(8, 'kanan hinta', 1),
+(9, 'kaalin hinta', 1);
 
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `vastaukset`
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$R.OkFmxsOBTUgyjGC/Yj9uLREfD3lf6dRTfGfMiHsdwt8bTZKtAcG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vastaukset`
 --
 
 DROP TABLE IF EXISTS `vastaukset`;
 CREATE TABLE IF NOT EXISTS `vastaukset` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Vastaus` int(11) NOT NULL,
+  `Vastaus` varchar(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vastaukset`
+--
+
+INSERT INTO `vastaukset` (`ID`, `Vastaus`) VALUES
+(1, 'kogfk'),
+(2, 'fffff'),
+(3, 'kkkkk'),
+(18, ''),
+(6, 'fgfdgf'),
+(7, 'gffgfgfg'),
+(9, 'ertert');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
